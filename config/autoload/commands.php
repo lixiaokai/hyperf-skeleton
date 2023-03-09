4f29be1f9c154e2d8dc17a6a2b7a6fedee687c09
lixiaokai <lixiaokai@live.com>
2023-03-09 14:41:01 +0800

    完善：命令行配置文件注释

--- config/autoload/commands.php
+++ config/autoload/commands.php
@@ -1,13 +1,11 @@
 <?php
 
 declare(strict_types=1);
+
 /**
- * This file is part of Hyperf.
+ * 命令行 - 配置文件.
  *
- * @link     https://www.hyperf.io
- * @document https://hyperf.wiki
- * @contact  group@hyperf.io
- * @license  https://github.com/hyperf/hyperf/blob/master/LICENSE
+ * @see https://hyperf.wiki/3.0/#/zh-cn/command
  */
 return [
 ];

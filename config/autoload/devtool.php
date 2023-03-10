9a7215ec2c556401126fcc9c3b776962c3c6a171
lixiaokai <lixiaokai@live.com>
2023-03-10 11:49:31 +0800

    增加：开发者工具配置中的 API 资源和控制器模板

--- config/autoload/devtool.php
+++ config/autoload/devtool.php
@@ -5,38 +5,48 @@ declare(strict_types=1);
 /**
  * 开发者工具 - 配置.
  *
+ * 说明：
+ * 1. namespace 表示默认命名空间，生成的文件会根据命名空间对应路径来保存
+ * 2. 也可以通过增加参数 -N 'App\Admin\Controller' 修改命名空间
+ *
  * @see https://hyperf.wiki/3.0/#/zh-cn/devtool
  */
 return [
     'generator' => [
         'amqp' => [
             'consumer' => [
-                'namespace' => 'App\\Amqp\\Consumer',
+                'namespace' => 'Core\\Amqp\\Consumer',
             ],
             'producer' => [
-                'namespace' => 'App\\Amqp\\Producer',
+                'namespace' => 'Core\\Amqp\\Producer',
             ],
         ],
         'aspect' => [
-            'namespace' => 'App\\Aspect',
+            'namespace' => 'Core\\Aspect',
         ],
         'command' => [
-            'namespace' => 'App\\Command',
+            'namespace' => 'Core\\Command',
         ],
         'controller' => [
-            'namespace' => 'App\\Controller',
+            'namespace' => 'App\\Admin\\Controller',
+            'stub' => BASE_PATH . '/core/Devtool/Generator/stubs/controller.stub',
+            // 'stub' => BASE_PATH . '/vendor/hyperf/devtool/src/Generator/stubs/controller.stub', // 默认模板
         ],
         'job' => [
-            'namespace' => 'App\\Job',
+            'namespace' => 'Core\\Job',
         ],
         'listener' => [
-            'namespace' => 'App\\Listener',
+            'namespace' => 'Core\\Listener',
         ],
         'middleware' => [
-            'namespace' => 'App\\Middleware',
+            'namespace' => 'Core\\Middleware',
         ],
         'Process' => [
-            'namespace' => 'App\\Processes',
+            'namespace' => 'Core\\Processes',
+        ],
+        /* @see \Hyperf\Devtool\Generator\ResourceCommand 默认配置中没有该 API 资源配置，以下是自己加上的 */
+        'resource' => [
+            'namespace' => 'App\\Admin\\Resource',
         ],
     ],
 ];

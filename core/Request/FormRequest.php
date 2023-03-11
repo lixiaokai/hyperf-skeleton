e823cc8d0b95ad37e8aeb2f3c747c1ba5d5732ea
lixiaokai <lixiaokai@live.com>
2023-03-11 17:19:32 +0800

    增加：常用验证规则注释

--- core/Request/FormRequest.php
+++ core/Request/FormRequest.php
@@ -11,4 +11,24 @@ use Kernel\Request\AbstractFormRequest;
  */
 class FormRequest extends AbstractFormRequest
 {
+    /**
+     * 常用 - 验证规则.
+     *
+     * ```
+     * bail     首次失败后停止后面规则的检查 ( 必须是首规则 )
+     * integer  大于 0 整形或字符串整形
+     * numeric  数值
+     *
+     * in:enable,disable           存在给定的列表中
+     * Rule::in(Status::codes())   如上
+     *
+     * exists:menu,id     存在于数据表
+     * exists:db.menu,id  指定数据库
+     * Rule::exists('menu', 'id')->where('status', Status::ENABLE)->whereNull('deleted_at')
+     * ```
+     */
+    public function rules(): array
+    {
+        return [];
+    }
 }

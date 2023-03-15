83264194f16d95cc64e59cf114e547dedf2e3cdb
lixiaokai <lixiaokai@live.com>
2023-03-15 10:01:56 +0800

    修改：用户表密码字段为类型转换

--- core/Model/User.php
+++ core/Model/User.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Core\Model;
 
 use Carbon\Carbon;
+use Core\Model\Casts\PasswordHash;
 use Core\Model\Traits\StatusTrait;
 use Core\Model\Traits\UserActionTrail;
 use Core\Model\Traits\UserAuthTrail;
@@ -14,13 +15,13 @@ use Core\Model\Traits\UserAuthTrail;
  *
  * 即所有用户的基础表
  *
- * @property int    $id        用户 ID
- * @property string $name      用户名
- * @property string $phone     手机号
- * @property string $password  密码
- * @property string $status    状态 ( enable-启用 disable-禁用 )
- * @property Carbon $createdAt 创建时间
- * @property Carbon $updatedAt 修改时间
+ * @property int     $id        用户 ID
+ * @property ?string $name      用户名
+ * @property string  $phone     手机号
+ * @property ?string $password  密码
+ * @property string  $status    状态 ( enable-启用 disable-禁用 )
+ * @property Carbon  $createdAt 创建时间
+ * @property Carbon  $updatedAt 修改时间
  */
 class User extends AbstractModel
 {
@@ -46,6 +47,7 @@ class User extends AbstractModel
 
     protected array $casts = [
         'id' => 'integer',
+        'password' => PasswordHash::class,
         'created_at' => 'datetime',
         'updated_at' => 'datetime',
     ];

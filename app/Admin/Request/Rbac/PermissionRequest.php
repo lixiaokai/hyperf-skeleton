a01edd9e791647bdcfc5175cff732b498e22dcd4
lixiaokai <lixiaokai@live.com>
2023-03-20 16:52:55 +0800

    完善：权限管理验证

--- app/Admin/Request/Rbac/PermissionRequest.php
+++ app/Admin/Request/Rbac/PermissionRequest.php
@@ -4,7 +4,10 @@ declare(strict_types=1);
 
 namespace App\Admin\Request\Rbac;
 
+use Core\Model\Permission;
 use Core\Request\FormRequest;
+use Hyperf\Validation\Rule;
+use Hyperf\Validation\Rules\Exists;
 
 /**
  * 权限 - 创建|修改 - 请求类.
@@ -21,24 +24,34 @@ class PermissionRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'parentId' => ['bail', 'nullable', 'integer'],
+            'parentId' => ['bail', 'integer', $this->parentIdExists()],
             'route' => ['bail', 'required', 'string', 'max:100'],
             'attachRoutes' => ['bail'],
             'name' => ['bail', 'required', 'string', 'max:100'],
             'desc' => ['bail', 'string', 'max:250'],
-            'sort' => ['bail', 'integer', 'max:9999'],
+            'sort' => ['bail', 'integer', 'between:0,9999'],
         ];
     }
 
     public function attributes(): array
     {
         return [
-            'parentId' => '父 ID',
+            'parentId' => '上级权限',
             'route' => '路由',
             'attachRoutes' => '附加路由',
             'name' => '名称',
             'desc' => '描述',
             'sort' => '排序',
         ];
+    }
+
+    protected function parentIdExists(): ?Exists
+    {
+        $parentId = $this->input('parentId');
+        if ($parentId > 0) {
+            return Rule::exists(Permission::table(), 'id');
+        }
+
+        return null;
     }
 }

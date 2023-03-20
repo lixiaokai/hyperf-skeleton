d9fff7cca73bdbcb73c38d88d6a154aa4edea0f4
lixiaokai <lixiaokai@live.com>
2023-03-20 14:28:18 +0800

    修复：PhpStorm IDE 提示

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,10 +2,16 @@
 
 namespace PHPSTORM_META {
     // Reflect
+    use Core\Constants\ContextKey;
+
     override(\Psr\Container\ContainerInterface::get(0), map(['' => '@']));
     override(\Hyperf\Context\Context::get(), map([
         'user' => \Core\Model\User::class,
+        ContextKey::USER => \Core\Model\User::class,
+
         'userAdmin' => \Core\Model\UserAdmin::class,
+        ContextKey::USER_ADMIN => \Core\Model\UserAdmin::class,
+
         '' => '@'
     ]));
     override(\make(0), map(['' => '@']));

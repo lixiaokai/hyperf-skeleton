083ed80aac9d881d70e646c5f1ddb054ed00c789
lixiaokai <lixiaokai@live.com>
2023-03-21 11:07:41 +0800

    增加：字节数格式化方法

--- kernel/Helper/FormatHelper.php
+++ kernel/Helper/FormatHelper.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Kernel\Helper;
 
 use Carbon\Carbon;
@@ -23,5 +25,23 @@ class FormatHelper
     public static function toDateTimeString(?Carbon $dt, ?string $default = ''): string
     {
         return $dt?->toDateTimeString() ?? $default;
+    }
+
+    /**
+     * 字节数格式化.
+     *
+     * @param  int    $bytes    文件大小 ( 字节数 )
+     * @param  int    $decimals 保留多少位小数
+     * @return string 带单位的文件大小字符串
+     */
+    public static function formatBytes(int $bytes, int $decimals = 2): string
+    {
+        $units = ['B', 'kB', 'MB', 'GB', 'TB', 'PB'];
+        $key = floor((strlen((string) $bytes) - 1) / 3); // 舍去法取整
+
+        $unit = @$units[$key]; // 单位
+        $fileSize = sprintf("%.{$decimals}f", $bytes / pow(1024, $key)); // 字节大小
+
+        return $fileSize . ' ' . $unit;
     }
 }

7530648df5f9729dacd534a3b4d0ec75cc5ba18e
lixiaokai <lixiaokai@live.com>
2023-03-28 17:12:10 +0800

    完善：字段注释

--- core/Model/RoleUser.php
+++ core/Model/RoleUser.php
@@ -9,13 +9,13 @@ use Carbon\Carbon;
 /**
  * 角色用户关系 - 模型.
  *
- * 角色、用户、租户、应用关系
+ * 增加租户 ID、应用 ID 作为冗余字段，目的是方便一些查询
  *
  * @property int    $id        自增 ID
  * @property int    $roleId    角色 ID
  * @property int    $userId    用户 ID
- * @property int    $tenantId  租户 ID
- * @property string $appId     应用 ID
+ * @property int    $tenantId  租户 ID ( 冗余 )
+ * @property string $appId     应用 ID ( 冗余 )
  * @property Carbon $createdAt 创建时间
  */
 class RoleUser extends AbstractModel

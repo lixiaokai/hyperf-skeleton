3fa55de696288c1e37a327e4929499dac4cecbd0
lixiaokai <lixiaokai@live.com>
2023-03-28 15:45:27 +0800

    增加：租户状态常量

--- core/Model/Tenant.php
+++ core/Model/Tenant.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Core\Model;
 
 use Carbon\Carbon;
+use Core\Constants\TenantStatus;
 use Core\Model\Traits\StatusTrait;
 use Core\Model\Traits\TenantActionTrail;
 
@@ -14,9 +15,11 @@ use Core\Model\Traits\TenantActionTrail;
  * @property int    $id        租户 ID
  * @property string $name      租户名称
  * @property ?array $data      租户数据
- * @property string $status    状态 ( init-初始化 enable-启用 disable-禁用 )
+ * @property string $status    状态 ( waitInit-待初始化 enable-启用 disable-禁用 )
  * @property Carbon $createdAt 创建时间
  * @property Carbon $updatedAt 修改时间
+ *
+ * @property string $statusText 状态 - 文字
  */
 class Tenant extends AbstractModel
 {
@@ -28,4 +31,9 @@ class Tenant extends AbstractModel
     protected array $fillable = ['id', 'name', 'data', 'status', 'created_at', 'updated_at'];
 
     protected array $casts = ['id' => 'integer', 'data' => 'json', 'created_at' => 'datetime', 'updated_at' => 'datetime'];
+
+    public function getStatusTextAttribute(): string
+    {
+        return TenantStatus::getText($this->status);
+    }
 }

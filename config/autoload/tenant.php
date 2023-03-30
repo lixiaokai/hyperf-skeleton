10a1407be6fceaee83658f6177a5896d5a19489d
lixiaokai <lixiaokai@live.com>
2023-03-30 10:06:27 +0800

    完善：租户配置注释

--- config/autoload/tenant.php
+++ config/autoload/tenant.php
@@ -3,14 +3,15 @@
 declare(strict_types=1);
 
 /**
- * 租户配置.
+ * 单租户配置.
  *
- * @see https://github.com/overtrue/easy-sms
+ * 应用和租户映射配置
  */
 use Core\Constants\AppId;
 
 return [
+    // 总后台为单租户模式，锁死租户
     AppId::ADMIN => [
-        'id' => 1, // 总后台应用租户 ID
+        'id' => 1, // 租户 ID
     ],
 ];
